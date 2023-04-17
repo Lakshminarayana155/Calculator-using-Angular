f691cbc969b3a7ff7390eaee0d7b83785b7924d8
[LakshmiNarayana M] <lakshminarayanamanchuri@gmail.com>
2023-04-17 14:12:38 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mycalculator';
+  toshow='0';
+  currentval=""
+  writetoinput(value:string){
+    if (this.currentval=='0')
+    {
+      this.currentval=value
+    }else{
+      this.currentval+=value
+    }
+    this.toshow=this.currentval
+  }
+
+  clear(){
+    this.currentval=''
+    this.toshow='0'
+  }
+
+  equals(){
+    this.toshow=eval(this.currentval)
+    this.currentval=this.toshow
+  }
+
+  back(){
+     this.currentval=this.currentval.slice(0,-1)
+     if (this.currentval==""){
+      this.currentval='0'
+     }
+     this.toshow=this.currentval
+  }
+
+  calcval(solve:any){
+    if(solve.charAt(0)=='0'){
+      this.toshow= eval(solve.slice(1,))
+    }
+    else{
+      this.toshow= eval(solve)
+    }
+    
+  }
 }
